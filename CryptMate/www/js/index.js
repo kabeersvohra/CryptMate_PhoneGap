789358eaee914877ac45a401ca0d21efee9b99d5
KVohra95 <kabeer@vohra.eu>
2015-09-28 15:34:40 +0100

    got everything working except login

--- CryptMate/www/js/index.js
+++ CryptMate/www/js/index.js
@@ -17,9 +17,8 @@
  * under the License.
  */
 
-var url = "";
-var domain = "";
 var token = "";
+var domain = "";
 var newpassword = "";
 var subscriptionended = false;
 var params = {};
@@ -31,11 +30,13 @@ var createnewpassworddiv = $("#createnewpassword");
 var generatepassworddiv = $("#generatepassword");
 var showpassworddiv = $("#showpassword");
 var maindiv = $("#main");
-
+var domaininput = $("#domain");
 var errordiv = $("#error");
 var formsdiv = $("#forms");
 var linkdomains = $("#linkdomains");
-
+var storeddomains = $("#storeddomains");
+var generatebutton = $("#generate");
+var createbutton = $("#create");
 
 var app = {
     // Application Constructor
@@ -56,18 +57,16 @@ var app = {
     onDeviceReady: function() {
         $("form").on('submit', function (e) {
             switch (e.target.id) {
-                case "selectform":
-                    alert(e.target.value);
-                    break;
                 case "createnewpassword":
                     var newpassword = newpasswordform.val();
                     var confirmpassword = confirmpasswordform.val();
+                    domain = domaininput.val();
                     var linkeddomain = linkdomains.find("option:selected").text();
 
                     if (newpassword == confirmpassword) {
                         params =
                         {
-                            token: token,
+                            token: "HCoUQ9yGbjSP4iyLLAClrXCVbh3Uc2ZHuds9cOFbVlROrdq2BScSDFDCKtkKl0iDbyBbc5cYgRCvUQmlwn2ZStpqMz2Xx0qyxSxxMxjQfKcXqo8NBYAhfQySdnFAkUWFAj3cFcRIKTv16qBvf1CkGY1JbuajeUOE3FExFl6f5o6YFvjIlLSPyJox4mH66lzXQ2klddq6rkTWD3uOCbr1IFnzQUuL7RyKIGWLJaFYkoLLh4pH3GxAaKZOvhnpYLXx",
                             password: newpassword,
                             domain: domain,
                             newpassword: 1,
@@ -104,9 +103,10 @@ var app = {
                     break;
                 case "generatepassword":
                     var password = passwordform.val();
+                    domain = storeddomains.find("option:selected").text();
                     params =
                     {
-                        token: token,
+                        token: "HCoUQ9yGbjSP4iyLLAClrXCVbh3Uc2ZHuds9cOFbVlROrdq2BScSDFDCKtkKl0iDbyBbc5cYgRCvUQmlwn2ZStpqMz2Xx0qyxSxxMxjQfKcXqo8NBYAhfQySdnFAkUWFAj3cFcRIKTv16qBvf1CkGY1JbuajeUOE3FExFl6f5o6YFvjIlLSPyJox4mH66lzXQ2klddq6rkTWD3uOCbr1IFnzQUuL7RyKIGWLJaFYkoLLh4pH3GxAaKZOvhnpYLXx",
                         password: password,
                         domain: domain,
                         newpassword: 0
@@ -151,14 +151,68 @@ document.getElementById("copy").addEventListener("click", function ()
     document.execCommand("Copy", false, null);
 });
 
-function generate() {
-    alert("reached");
-    maindiv.show();
-    generatepassworddiv.show();
+function populateDropdown (keyeddomains) {
+
 }
 
-function create() {
-    alert("reached");
-    maindiv.show();
-    generatepassworddiv.show();
+window.onload = function() {
+
+    generatebutton.click( function() {
+        maindiv.show();
+        generatepassworddiv.show();
+    });
+
+    createbutton.click( function() {
+        maindiv.show();
+        createnewpassworddiv.show();
+    });
+
+    params =
+    {
+        token: "HCoUQ9yGbjSP4iyLLAClrXCVbh3Uc2ZHuds9cOFbVlROrdq2BScSDFDCKtkKl0iDbyBbc5cYgRCvUQmlwn2ZStpqMz2Xx0qyxSxxMxjQfKcXqo8NBYAhfQySdnFAkUWFAj3cFcRIKTv16qBvf1CkGY1JbuajeUOE3FExFl6f5o6YFvjIlLSPyJox4mH66lzXQ2klddq6rkTWD3uOCbr1IFnzQUuL7RyKIGWLJaFYkoLLh4pH3GxAaKZOvhnpYLXx",
+        keyeddomains: true
+    };
+
+    $.ajax({
+        type: "POST",
+        url: "https://www.cryptmate.com/processing/rest.php",
+        data: params,
+        crossDomain: true,
+        cache: false,
+        success: function (data, status) {
+            var keyeddomains = JSON.parse(data);
+
+            for(var i = 0; i < keyeddomains.length; i++)
+            {
+                var opt1 = document.createElement('option');
+                opt1.innerHTML = keyeddomains[i];
+                opt1.value = keyeddomains[i];
+                document.getElementById('storeddomains').appendChild(opt1);
+                var opt2 = document.createElement('option');
+                opt2.innerHTML = keyeddomains[i];
+                opt2.value = keyeddomains[i];
+                document.getElementById('linkdomains').appendChild(opt2);
+            }
+        },
+        error: function () {
+            error("Internet connection failure, please try again when internet connection is active")
+        }
+    });
+
+};
+
+function clearAll()
+{
+    passwordform.value = "";
+    newpasswordform.value = "";
+    confirmpasswordform.value = "";
+    generatedpasswordform.value = "";
+}
+
+function error(message)
+{
+    clearAll();
+    errordiv.html(message);
+    formsdiv.hide();
+    errordiv.show();
 }
